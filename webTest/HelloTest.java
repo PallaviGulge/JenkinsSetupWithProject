146959aec8a3deaf4348aedddfe33bbb1c99f4fe
PallaviGulge <pallavigulge272@gmail.com>
2023-11-26 14:25:50 +0530

    New 2:25

--- webTest/HelloTest.java
+++ webTest/HelloTest.java
@@ -4,6 +4,8 @@ public class HelloTest {
 
 	public static void main(String[] args) {
 		
+		System.out.println("This is simple loop demo");
+		
 		for(int i=1;i<=10;i++)
 		{
 			System.out.println("Hello");
